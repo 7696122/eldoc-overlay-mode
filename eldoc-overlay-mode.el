a140f40fd25709708743c8e4bf34cf430450728f
stardiviner <numbchild@gmail.com>
2017-01-19 11:30:38 +0800

    improve private variables and functions definition names.
    - require Emacs version 25.1

--- eldoc-overlay-mode.el
+++ eldoc-overlay-mode.el
@@ -5,7 +5,7 @@
 ;; URL: https://github.com/stardiviner/eldoc-overlay-mode
 ;; Created: 14th Jan 2017
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "25.1"))
 
 ;;; Commentary:
 
@@ -14,43 +14,44 @@
 ;;; Code:
 ;;; ----------------------------------------------------------------------------
 
-(defvar eldoc-overlay-display-overlay nil)
+(defvar eldoc-overlay--overlay nil)
 
 (defface eldoc-overlay-face
   '((t (:inherit italic)))
   "Face for eldoc-overlay-mode."
   :group 'eldoc)
 
-(defun eldoc-overlay-delete-string-display ()
-  (when (overlayp eldoc-overlay-display-overlay)
-    (delete-overlay eldoc-overlay-display-overlay))
-  (remove-hook 'post-command-hook 'eldoc-overlay-delete-string-display))
+(defun eldoc-overlay--clear-overlay ()
+  "Clear eldoc-overlay overlays."
+  (when (overlayp eldoc-overlay--overlay)
+    (delete-overlay eldoc-overlay--overlay))
+  (remove-hook 'post-command-hook 'eldoc-overlay--clear-overlay))
 
-(defun eldoc-overlay-string-display-next-line (string)
+(defun eldoc-overlay--string-display-next-line (string)
   "Overwrite contents of next line with STRING until next command."
   (let ((str (concat (make-string (1+ (current-indentation)) 32)
                      (copy-sequence string)))
         start-pos end-pos)
     (unwind-protect
         (save-excursion
-          (eldoc-overlay-delete-string-display)
+          (eldoc-overlay--clear-overlay)
           (forward-line)
           (setq start-pos (point))
           (end-of-line)
           (setq end-pos (point))
-          (setq eldoc-overlay-display-overlay (make-overlay start-pos end-pos))
+          (setq eldoc-overlay--overlay (make-overlay start-pos end-pos))
           ;; Change the face
-          (overlay-put eldoc-overlay-display-overlay 'face 'eldoc-overlay-face)
+          (overlay-put eldoc-overlay--overlay 'face 'eldoc-overlay-face)
           ;; Hide full line
-          (overlay-put eldoc-overlay-display-overlay 'display "")
+          (overlay-put eldoc-overlay--overlay 'display "")
           ;; Display message
-          (overlay-put eldoc-overlay-display-overlay 'before-string str))
-      (add-hook 'post-command-hook 'eldoc-overlay-delete-string-display))))
+          (overlay-put eldoc-overlay--overlay 'before-string str))
+      (add-hook 'post-command-hook 'eldoc-overlay--clear-overlay))))
 
 (defun eldoc-overlay-display-message-momentary (format-string &rest args)
-  "Display eldoc message near point."
+  "Display eldoc message FORMAT-STRING near point with extra ARGS."
   (when format-string
-    (eldoc-overlay-string-display-next-line
+    (eldoc-overlay--string-display-next-line
      (apply 'format format-string args))))
 
 (defvar eldoc-overlay-mode-map
